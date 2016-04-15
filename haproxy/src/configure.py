8c2f26df4adf3b3bd1f08e4d879b00b5384d7532
Alin Voinea <alin@eaudeweb.ro>
2016-04-15 15:46:59 +0300

    Refactoring

--- haproxy/src/configure.py
+++ haproxy/src/configure.py
@@ -2,6 +2,10 @@ import os
 import socket
 import sys
 
+################################################################################
+# INIT
+################################################################################
+
 FRONTEND_NAME = os.environ.get('FRONTEND_NAME', 'http-frontend')
 FRONTEND_PORT = os.environ.get('FRONTEND_PORT', '5000')
 BACKEND_NAME = os.environ.get('BACKEND_NAME', 'http-backend')
@@ -46,6 +50,82 @@ backend_conf_plus = """
     server http-server%(index)d %(host)s:%(port)s %(cookies)s check
 """
 
+if COOKIES_ENABLED:
+    cookies = "cookie value"
+else:
+    cookies = ""
+
+backend_conf = backend_conf % dict(backend=BACKEND_NAME, balance=BALANCE)
+
+################################################################################
+# Look for backend within /etc/hosts
+################################################################################
+if sys.argv[1] == "hosts":
+    try:
+        hosts = open("/etc/hosts")
+    except:
+        exit(0)
+
+    index = 1
+    localhost = socket.gethostbyname(socket.gethostname())
+    existing_hosts = set()
+
+    #BBB
+    if ';' in SERVICE_NAMES:
+        service_names = SERVICE_NAMES.split(';')
+    else:
+        service_names = SERVICE_NAMES.split()
+
+    for host in hosts:
+        if "0.0.0.0" in host:
+            continue
+        if "127.0.0.1" in host:
+            continue
+        if localhost in host:
+            continue
+        if "::" in host:
+            continue
+
+        part = host.split()
+        if len(part) < 2:
+            continue
+
+        (host_ip, host_name) = part[0:2]
+        if host_ip in existing_hosts:
+            continue
+
+        if service_names and not any(name in host_name for name in service_names):
+            continue
+
+        existing_hosts.add(host_ip)
+        host_port = BACKENDS_PORT
+        backend_conf += backend_conf_plus % dict(
+                index=index,
+                host=host_ip,
+                port=host_port,
+                cookies=cookies
+        )
+        index += 1
+
+################################################################################
+# Backends provided via BACKENDS environment variable
+################################################################################
+if sys.argv[1] == "env":
+    for index, backend_server in enumerate(BACKENDS):
+        server_port = backend_server.split(':')
+        host = server_port[0]
+        port = server_port[1] if len(server_port) > 1 else BACKENDS_PORT
+        backend_conf += backend_conf_plus % dict(
+                index=index,
+                host=host,
+                port=port,
+                cookies=cookies)
+
+if PROXY_PROTOCOL_ENABLED:
+    accept_proxy = "accept-proxy"
+else:
+    accept_proxy = ""
+
 with open("/etc/haproxy/haproxy.cfg", "w") as configuration:
 
     with open("/tmp/haproxy.cfg", "r") as default:
@@ -53,76 +133,6 @@ with open("/etc/haproxy/haproxy.cfg", "w") as configuration:
         if LOGGING:
             conf = conf.replace('127.0.0.1', LOGGING)
         configuration.write(conf)
-
-    backend_conf = backend_conf % dict(backend=BACKEND_NAME, balance=BALANCE)
-
-    if COOKIES_ENABLED:
-        cookies = "cookie value"
-    else:
-        cookies = ""
-
-    if PROXY_PROTOCOL_ENABLED:
-        accept_proxy = "accept-proxy"
-    else:
-        accept_proxy = ""
-
-    if ';' in SERVICE_NAMES:
-        #BBB
-        service_names = SERVICE_NAMES.split(';')
-    else:
-        service_names = SERVICE_NAMES.split()
-
-    if sys.argv[1] == "hosts":
-        try:
-            hosts = open("/etc/hosts")
-        except:
-            exit(0)
-
-        index = 1
-        localhost = socket.gethostbyname(socket.gethostname())
-        existing_hosts = set()
-
-        for host in hosts:
-            if "0.0.0.0" in host:
-                continue
-            if "127.0.0.1" in host:
-                continue
-            if localhost in host:
-                continue
-            if "::" in host:
-                continue
-
-            part = host.split()
-            if len(part) < 2:
-                continue
-
-            (host_ip, host_name) = part[0:2]
-            if host_ip in existing_hosts:
-                continue
-
-            if service_names and not any(name in host_name for name in service_names):
-                continue
-
-            existing_hosts.add(host_ip)
-            host_port = BACKENDS_PORT
-            backend_conf += backend_conf_plus % dict(
-                    index=index,
-                    host=host_ip,
-                    port=host_port,
-                    cookies=cookies
-            )
-            index += 1
-
-    if sys.argv[1] == "env":
-        for index, backend_server in enumerate(BACKENDS):
-            server_port = backend_server.split(':')
-            host = server_port[0]
-            port = server_port[1] if len(server_port) > 1 else BACKENDS_PORT
-            backend_conf += backend_conf_plus % dict(
-                    index=index,
-                    host=host,
-                    port=port,
-                    cookies=cookies)
 
     configuration.write(listen_conf % dict(port=STATS_PORT, auth=STATS_AUTH))
     configuration.write(frontend_conf % dict(

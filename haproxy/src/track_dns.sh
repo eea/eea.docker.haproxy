5c51f780d15f3fbd65c5dba519f929c32d80fc2d
Alin Voinea <alin@eaudeweb.ro>
2016-04-15 18:13:08 +0300

    Fix track_dns script

--- haproxy/src/track_dns.sh
+++ haproxy/src/track_dns.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-IPS_1 = `cat /etc/haproxy/dns.backends`
+IPS_1=`cat /etc/haproxy/dns.backends`
 python3 /configure.py dns
-IPS_2 = `cat /etc/haproxy/dns.backends`
+IPS_2=`cat /etc/haproxy/dns.backends`
 
 if [ "$IPS_1" != "$IPS_2" ]; then
   echo "DNS backends changed: $IPS_2"
